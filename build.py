de457398d7b492e331f636ca7124ada7c7f45391
zyx <whyzi@qq.com>
2018-08-16 14:31:53 +0800

    zyx

---
+++ build.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python2.7
+# coding=utf-8
+# vim:expandtab:ts=4:sw=4:
+# 
+import time
+import re
+import os
+import sys
+import shutil
+
+curpath=os.getcwd();#当前文件目录
+binfile=os.path.join(curpath,"bin")
+def clean():
+    if os.path.exists( binfile ):
+        print "  remove local assets: %s" % binfile 
+        shutil.rmtree( binfile ,True)
+
+def buildfrp(name):
+    frpspath=os.path.join(curpath,"cmd/"+name)
+    os.chdir( frpspath )
+    ret = os.system( "go build" )
+    if ret != 0:
+        print "build "+name+" Error!"
+        return
+    print "build "+name+" success!"
+    target=os.path.join(binfile,name+"/")
+    if os.path.exists( target )==False:
+        os.makedirs(target)
+    shutil.copyfile(os.path.join(frpspath,name+".exe"),os.path.join(target,name+".exe"))
+    print "copy "+name+".exe success!"
+    os.chdir( curpath )
+
+
+if __name__=="__main__":
+   clean()
+   buildfrp("frps")
+   buildfrp("frpc")
